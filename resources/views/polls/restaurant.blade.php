a0a084c0a6a5e986a7948bba961fd7e8ec25ad1d
Collier Devlin <jocodev@gmail.com>
2015-07-19 14:19:32 +0000

    fix restaurants

--- resources/views/polls/restaurant.blade.php
+++ resources/views/polls/restaurant.blade.php
@@ -1,39 +1,35 @@
 @extends('layouts.master')
 
 @section('content')
+<div class='row'>
+    <div class='col-sm-3'></div>
+    <div class='col-sm-6'>
 <form action='/polls/{{ $poll->id }}/restaurant/store' method='POST'>
     {!! csrf_field() !!}
     @if($errors->any())
     <h4>{{$errors->first()}}</h4>
     @endif
-    <div class='row'>
-    <div class='col-sm-6'>
+
         <div class='panel panel-default'>
             <div class='panel-heading'>
                 Add Restaurant
             </div>
-            <div class='panel-body'>  
-                <table class='table grid'>
-                    <tr>
-                        <td><label>Restaurant</label></td>
-                        <td>
-                            <select name='restaurant_id' class='form-control'>
-                                @foreach ($restaurants as $restaurant)
-                                    <option value='{{$restaurant->id}}'>{{$restaurant->name}}</option>
-                                @endforeach
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><input type='submit' class='btn btn-success btn-outline' value='Save' /></td>
-                    </tr>
-                </table>
+            <div class='panel-body'>
+                <div class='form-group'>
+                    <label>Restaurant</label>
+                    <select name='restaurant_id' class='form-control'>
+                        @foreach ($restaurants as $restaurant)
+                            <option value='{{$restaurant->id}}'>{{$restaurant->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class='form-group'>
+                    <input type='submit' class='form-control btn btn-success' value='Save' /></td>
+                </div>
             </div>
         </div>
     </div>
-    </div>
-    
-    
+    <div class='col-sm-3'>
+</div>
 </form>
 @endsection
