04e62be56d6ed144ba6c7521aa5dc56841bd2050
Alex Sears <alexwsears@gmail.com>
2015-07-19 10:09:08 -0400

    Added route to send order message to slack channel

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Organization;
+use App\OrganizationOrder;
 use App\Poll;
 
 class SlackController extends Controller
@@ -16,6 +17,20 @@ class SlackController extends Controller
         $message = "Here's today's choices for lunch: ";
         $message .= $poll->restaurants->implode('name', ', ');
         $message .= ". Make sure to `/vote` your response!";
+        $body = json_encode(['text' => $message]);
+
+        $client = new \GuzzleHttp\Client();
+        $res = $client->post($organization->incoming_slack_link, ['body' => $body]);
+
+        return redirect()->route('organization.view', [$organization->id]);
+    }
+
+    public function outgoingOrder(Request $request, $id) {
+        $organization = Organization::findOrFail($id);
+        $order = OrganizationOrder::orderBy('created_at', 'desc')->first();
+
+        $message = "Today's lunch will be " . $order->restaurant()->name . "!";
+        $message .= " Make sure to `/order` your response! Send `/order default` to choose your default order.";
         $body = json_encode(['text' => $message]);
 
         $client = new \GuzzleHttp\Client();
