9fccef921cd9804f97cfa5c6b278e64ab1f804ee
Collier Devlin <jocodev@gmail.com>
2015-07-19 15:51:08 +0000

    some final changes

--- resources/views/userOrders/form.blade.php
+++ resources/views/userOrders/form.blade.php
@@ -1,41 +1,44 @@
 @extends('layouts.master')
 
 @section('content')
-<form action='/user-orders' method='POST'>
-    {!! csrf_field() !!}
-    @if($errors->any())
-    <h4>{{$errors->first()}}</h4>
-    @endif
-    <div class='row'>
-        <div class='col-sm-6'>
-            <div class='panel panel-default'>
-                <div class='panel-heading'>
-                    Add Order
-                </div>
-                <div class='panel-body'>
-                <table class='table grid'>
-                    <tr>
-                        <td class='form-left'><label>Choose Restaurant</label></td>
-                        <td class='form-right'><select class="form-control" name='UserOrder[restaurant_id]'>
-                            @foreach ($restaurants as $restaurant)
-                            <option value='{{ $restaurant->id }}'>{{ $restaurant->name }}</option>
-                            @endforeach
-                        </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label>Your Order</label></td>
-                        <td><input type='text' class="form-control" name="UserOrder[order]" value="{{ $user_order->order }}" /><br></td>
-                    </tr>
-                    <tr>
-                        <td><label>Make Default</label></td>
-                        <td><input type='checkbox' name="UserOrder[default]" value="1" /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><input type='submit' class='btn btn-success btn-outline' value='Save' /></input</td>
-                    </tr>
-                </table>
-                </div>
-</form>
+<h1 class='centerh1'>Add Order</h1>
+<div class='row'>
+    <div class='col-sm-3'></div>
+    <div class='col-sm-6'>
+        <form action='/user-orders' method='POST'>
+            {!! csrf_field() !!}
+            @if($errors->any())
+            <h4>{{$errors->first()}}</h4>
+            @endif
+                    <div class='panel panel-default' style="margin-top: 5px;">
+                        <div class='panel-heading'>
+                            Order Details
+                        </div>
+                        <div class='panel-body'>
+                        <div class='form-group'>
+                            <label>Choose Restaurant</label>
+                            <select class="form-control" name='UserOrder[restaurant_id]'>
+                                @foreach ($restaurants as $restaurant)
+                                <option value='{{ $restaurant->id }}'>{{ $restaurant->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class='form-group'>
+                            <label>Your Order</label>
+                            <input type='text' class="form-control" name="UserOrder[order]" value="{{ $user_order->order }}" />
+                        </div>
+                        <div class='form-group'>
+                            <label>Make Default</label><br>
+                            <select class="form-control" name="UserOrder[default]">
+                                <option value='0'>No</option>
+                                <option value='1'>Yes</option>
+                            </select>
+                        </div>
+                        <div class='form-group'>     
+                            <input type='submit' class='btn btn-success form-control' value='Save' /></input>
+                        </div>
+                    </div>
+        </form>
+    <div class='col-sm-3'></div>       
+</div>
 @endsection
--- resources/views/userOrders/list.blade.php
+++ resources/views/userOrders/list.blade.php
@@ -1,43 +1,32 @@
 @extends('layouts.master')
 
 @section('content')
-
-<a href='user-orders/create' class='btn btn-success btn-outline'><i class='fa fa-user-plus'></i> Create New Order</a>
-<br><br>
 <div class='row'>
-     <div class='col-sm-6'>
-         <div class='panel panel-default'>
-             <div class='panel-heading'>
-                 Filters
-             </div>
-             <div class='panel-body'>
-                <form name="filter" action='/user-orders' method='get'>
-                    <label for='restaurant'>Restaurant</label>
-                    <select id="restaurant-filter" name='restaurant' class="form-control" onchange='this.form.submit()'>
-                        <option value="">All</option>
-                        @foreach ($restaurants as $restaurant)
-                            <option value='{{ $restaurant->id }}'
-                                    {{ $selected_restaurant == $restaurant->id ? "selected" : "" }}>{{ $restaurant->name }}</option>
-                        @endforeach
-                    </select>
-                    <noscript><input type='submit' class='btn btn-success btn-outline' name='submit' value='Filter'/></noscript>
-                </form>                 
-             </div>
+
+<div class='col-sm-3'>
+    <a href='user-orders/create' class='btn btn-success btn-outline'><i class='fa fa-user-plus'></i> Create New Order</a>
+</div>
+<div class='col-sm-6'>
+    <h1 class='centerh1'>My Orders</h1>
+</div>
+<div class='col-sm-3'>
+         <div class='panel-body'>
+            <form name="filter" action='/user-orders' method='get'>
+                <label for='restaurant'>Filter by Restaurant</label>
+                <select id="restaurant-filter" name='restaurant' class="form-control" onchange='this.form.submit()'>
+                    <option value="">All</option>
+                    @foreach ($restaurants as $restaurant)
+                        <option value='{{ $restaurant->id }}'
+                                {{ $selected_restaurant == $restaurant->id ? "selected" : "" }}>{{ $restaurant->name }}</option>
+                    @endforeach
+                </select>
+                <noscript><input type='submit' class='btn btn-success btn-outline' name='submit' value='Filter'/></noscript>
+            </form>                 
          </div>
      </div>
-</div>
+ </div>
 
-
-
-<div class='row'>
-     <div class='col-sm-12'>
-         <div class='panel panel-default'>
-             <div class='panel-heading'>
-                Orders
-             </div>
-             <div class='panel-body'>
-
-                <table class='table grid'>
+                <table class='table table-striped'>
                     <tr>
                         <th>Restaurant</th>
                         <th>Order</th>
@@ -51,19 +40,16 @@
                             <td>{{ $user_order->order }}</td>
                             <td align='center'>
                             @if ($user_order->default != 1)
-                                <a href="/user-orders/{{ $user_order->id }}/default">
-                                <img class="default unselected" src="/img/star.png" alt="DEFAULT"/>
+                                <a href="/user-orders/{{ $user_order->id }}/default" class="btn btn-primary btn-sm">
+                                Make Default
                                 </a>
                             @else
-                                <img class="default" src="/img/star.png" alt="DEFAULT"/>
+                                <i class='fa fa-check-circle success'></i> Default
                             @endif
                             </td>
                             <td>{{ $user_order->created_at }}</td>
                         </tr>
                     @endforeach
                 </table>
-             </div>
-         </div>
-     </div>
-</div>
+
 @endsection
